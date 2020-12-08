a6472ea506be6c63ff88983974f667498c63e6d1
Saviz yahyazadeh <yasa2103@student.iepscf-uccle.be>
2020-12-08 10:56:26 +0100

    TP05.ex2

--- exercice2.js
+++ exercice2.js
@@ -1,5 +1,5 @@
 function analyzeTemperature(quantity, data) {
-
+if(quantity>=0 && quantity<=10000){
   if(quantity=0)
   {
       return 0;
@@ -7,11 +7,14 @@ function analyzeTemperature(quantity, data) {
   let temp=data.split(" ");
    for(let i=0; i<temp.length;i++)
   {
+    if(temp[i]>-273 && temp[i]<5526){
      temp[i]=(Math.abs(temp[i]));
    }
+  }
    temp.sort();
    return temp[0];
 
   }
+}
  console.log(analyzeTemperature(5,"1 -2 -8 4 5"));
 
